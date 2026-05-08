3133e46ecfbeb9e8b9073ecbde936982813ec6ba
evgeniy020304 <evgeniinaumov04@gmail.com>
2026-05-09 02:42:05 +0300

    fix: исправлена ошибка футера, изменено название, добавлено редактирование профиля

--- frontend/src/pages/EditProfilePage/EditProfilePage.tsx
+++ frontend/src/pages/EditProfilePage/EditProfilePage.tsx
@@ -1,7 +1,31 @@
 import styles from './EditProfilePage.module.scss';
+import { EditProfileForm } from '@components';
+import { MOCK_USER, MOCK_USER_AVATAR, MOCK_USER_TAGS } from '../../mocks/users';
+
 
 export const EditProfilePage = () => {
-	return <section className={styles.container}></section>;
-};
+	const formData = {
+		name: MOCK_USER.name,
+		weight: MOCK_USER.weight,
+		height: MOCK_USER.height,
+		avatar: null,
+		avatarUrl: MOCK_USER_AVATAR.avatar,
+		preferences: MOCK_USER_TAGS.tags.map((tag : any) => tag.id),
+	};
 
-export default EditProfilePage;
+	const handleUpdateData = (key: string, value: unknown) => {
+		console.log(`Обновление поля ${key}:`, value);
+	};
+
+	const availablePreferences = MOCK_USER_TAGS.tags;
+
+	return (
+		<section className={styles.container}>
+			<EditProfileForm
+				data={formData}
+				updateData={handleUpdateData}
+				availablePreferences={availablePreferences}
+			/>
+		</section>
+	);
+};
