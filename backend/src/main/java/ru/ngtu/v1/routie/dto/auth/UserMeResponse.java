ee72426ac64d8e8ec9ed0b04a2cb4ed4fee868a3
Ap1kkk <apikmaster2@yandex.ru>
2026-06-03 22:49:04 +0300

    refact Добавил использование билдеров

--- backend/src/main/java/ru/ngtu/v1/routie/dto/auth/UserMeResponse.java
+++ backend/src/main/java/ru/ngtu/v1/routie/dto/auth/UserMeResponse.java
@@ -3,10 +3,12 @@ package ru.ngtu.v1.routie.dto.auth;
 import java.util.List;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserMeResponse {
