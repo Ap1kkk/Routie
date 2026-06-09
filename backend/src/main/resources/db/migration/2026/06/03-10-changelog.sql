8971dd83377d13ceb5148cc1e20e0a34e0ffb20e
Ap1kkk <apikmaster2@yandex.ru>
2026-06-09 15:21:12 +0300

    fix 146 Исправил миграцию

--- backend/src/main/resources/db/migration/2026/06/03-10-changelog.sql
+++ backend/src/main/resources/db/migration/2026/06/03-10-changelog.sql
@@ -3,12 +3,12 @@
 -- changeset Ap1kkk:2026-06-08-05-route-sessions
 CREATE TABLE route_sessions
 (
-    id                    UUID        NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
-    user_id               UUID        NOT NULL REFERENCES users (id) ON DELETE CASCADE,
-    route_id              UUID        NOT NULL REFERENCES routes (id) ON DELETE CASCADE,
-    status                VARCHAR(20) NOT NULL DEFAULT 'ACTIVE',
-    started_at            TIMESTAMPTZ NOT NULL DEFAULT now(),
-    finished_at           TIMESTAMPTZ,
+    id                     UUID        NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
+    user_id                UUID        NOT NULL REFERENCES routie_users (id) ON DELETE CASCADE,
+    route_id               UUID        NOT NULL REFERENCES routes (id) ON DELETE CASCADE,
+    status                 VARCHAR(20) NOT NULL DEFAULT 'ACTIVE',
+    started_at             TIMESTAMPTZ NOT NULL DEFAULT now(),
+    finished_at            TIMESTAMPTZ,
     total_duration_seconds BIGINT,
     total_distance_meters  INTEGER,
     avg_speed_kmh          DOUBLE PRECISION
