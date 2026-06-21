810620d5e2ff65d01ab9efb0122a939dbc590535
Evgeniy Naumov <evgeniinaumov04@gmail.com>
2026-06-21 14:33:34 +0300

    [FE] fix: исправление

--- frontend/src/components/AuthorizationForm/AuthorizationForm.tsx
+++ frontend/src/components/AuthorizationForm/AuthorizationForm.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Input } from '@ui';
 import {
